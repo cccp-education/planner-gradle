5567429cc7ca2fd4d0a57c3efcc79e6b18f7c016
cheroliv <cheroliv@users.noreply.github.com>
2026-08-30 04:32:33 +0200

    build: align conventions-plugin to 0.0.4

--- planner-plugin/build.gradle.kts
+++ planner-plugin/build.gradle.kts
@@ -2,9 +2,9 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 
 plugins {
     alias(libs.plugins.publish)
-    id("education.cccp.build.gradle-plugin") version "0.0.2"
-    id("education.cccp.build.publishing") version "0.0.2"
-    id("education.cccp.build.cucumber") version "0.0.2"
+    id("education.cccp.build.gradle-plugin") version "0.0.4"
+    id("education.cccp.build.publishing") version "0.0.4"
+    id("education.cccp.build.cucumber") version "0.0.4"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10"
 }
 
